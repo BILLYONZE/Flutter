bf4690806366172e011d2fc40431277fff5a5165
Billjoy <billjoy.kilai@gmail.com>
2024-08-01 14:56:26 +0300

    Adding string and radius

--- Rectangle.dart
+++ Rectangle.dart
@@ -1,10 +1,15 @@
 int length = 20;
 int width = 30;
 int areaOfRectangle (){
-int answer = length * width;
-return answer;
+int areaAnswer = length * width;
+return areaAnswer;
+}
+int perimeterOfRectangle(){
+  int perimeterAnswer = (2*length) + (2*width);
+  return perimeterAnswer;
 }
 void main (){
   areaOfRectangle();
-  print (areaOfRectangle());
+  print ("Area of a rectangle:${areaOfRectangle()}");
+  print ("Perimeter of a rectangle:${perimeterOfRectangle()}");
 }
